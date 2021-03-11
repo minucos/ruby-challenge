bd9ebd3cc79b449691d10e39d942efe6a2e9456c
Andrew Minucos <minucos@gmail.com>
2021-03-10 21:55:35 -0500

    add specs for Basket class

---
+++ spec/basket_spec.rb
@@ -0,0 +1,23 @@
+require 'basket'
+
+describe Basket do
+  subject(:basket) { Basket.new }
+  describe "#initalize" do
+    it "initalizes with an empty items array" do
+      expect(basket.items).to eq([])
+    end
+  end
+
+  describe "#add" do
+    it "takes a string as argument, and adds item to items array" do
+      basket.add('1, book, 12.49')
+
+      expect(basket.items).to include([1, 'book', 12.49, imported: false])
+    end
+    it "sets imported property to true if included in string" do
+      basket.add('1, imported chocolates, 5')
+      
+      expect(basket.items).to include([1, 'chocolates', 5.0, importer: true])
+    end
+  end
+end
