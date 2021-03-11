212d8a8b706e85afec6cc18ffc2da1d9f83bcf87
Andrew Minucos <minucos@gmail.com>
2021-03-11 00:37:54 -0500

    complete program.rb to take 3 x test inputs

--- program.rb
+++ program.rb
@@ -1,11 +1,34 @@
 require_relative "./lib/basket"
 
-b = Basket.new
+puts "Calculating Test Input 1\n"
+b1 = Basket.new
 
-File.open("input_2.txt","r") do |f|
+File.open("input_1.txt","r") do |f|
   f.each_line do |line|
-    b.add(line)
+    b1.add(line)
   end
 end
 
-b.print_receipt
+b1.print_receipt
+
+puts "\n\nCalculating Test Input 2\n"
+b2 = Basket.new
+
+File.open("input_2.txt","r") do |f|
+  f.each_line do |line|
+    b2.add(line)
+  end
+end
+
+b2.print_receipt
+
+puts "\n\nCalculating Test Input 3\n"
+b3 = Basket.new
+
+File.open("input_3.txt","r") do |f|
+  f.each_line do |line|
+    b3.add(line)
+  end
+end
+
+b3.print_receipt
