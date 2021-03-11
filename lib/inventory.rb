6ed43a2808cf5ffbc326ddf85a8073f4bb9c614b
Andrew Minucos <minucos@gmail.com>
2021-03-10 20:57:43 -0500

    add inventory hash

---
+++ lib/inventory.rb
@@ -0,0 +1,10 @@
+# Inventory hash is used to represent all items that could be purchased, with a
+# value set to the amount of tax a product attracts
+INVENTORY = {
+  'book' => 0,
+  'music cd' => 10,
+  'chocolate bar' => 0,
+  'box of chocolates' => 0,
+  'bottle of perfume' => 10,
+  'headache pills' => 0
+}
